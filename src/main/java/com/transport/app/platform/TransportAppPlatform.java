e9bbeaec3cd4b2e2a6409851c01e0889d1dcc063
SebastiaNCastro22 <u20181c241@upc.edu.pe>
2024-11-01 13:04:39 -0500

    feet: deplot

---
+++ src/main/java/com/transport/app/platform/TransportAppPlatform.java
@@ -0,0 +1,31 @@
+package com.transport.app.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class TransportAppPlatform {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransportAppPlatform.class, args);
+    }
+    @Configuration
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer(){
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
+}
